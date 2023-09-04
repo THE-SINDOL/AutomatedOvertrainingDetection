d84062be4ec025b421cd477425ddb217e0bccdb1
grvyscale <142821265+grvyscale@users.noreply.github.com>
2023-09-04 21:18:42 +0200

    Delete install-requirements.bat

--- install-requirements.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title ROD - Install
-call runtime\Scripts\activate
-call runtime\Scripts\python -m pip uninstall -y -r install.txt
-call runtime\Scripts\python -m pip install -r install.txt
-echo.
-echo PACKAGES INSTALLED
-pause
