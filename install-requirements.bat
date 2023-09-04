de284086e6859874a5463e990adfab162d59bbcb
grvyscale <142821265+grvyscale@users.noreply.github.com>
2023-09-04 20:37:23 +0200

    install-requirements.bat

--- install-requirements.bat
+++ install-requirements.bat
@@ -1,5 +1,5 @@
 @echo off
-title RAOD - Install
+title ROD - Install
 call runtime\Scripts\activate
 call runtime\Scripts\python -m pip uninstall -y -r install.txt
 call runtime\Scripts\python -m pip install -r install.txt
