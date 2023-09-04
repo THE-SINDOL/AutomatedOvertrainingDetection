b9948a1e3db9b167fe760045192c43252a615813
grvyscale <142821265+grvyscale@users.noreply.github.com>
2023-09-04 21:08:58 +0200

    Add files via upload

---
+++ config-editor.bat
@@ -0,0 +1,4 @@
+@echo off
+title ROD - Config Editor
+call runtime\Scripts\activate
+call runtime\Scripts\python -m _config-editor.py
---
+++ rod.bat
@@ -0,0 +1,5 @@
+@echo off
+title ROD - Realtime Overtraining Detection
+call runtime\Scripts\activate
+call runtime\Scripts\python.exe _rod.py
+pause
